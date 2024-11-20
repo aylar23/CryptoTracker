80817ce012aab9d422076b7dc3d6fc93e35824b4
aylar23 <aylarbayram23@gmail.com>
2024-11-20 18:35:22 +0500

    Add Coin List UI

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -0,0 +1,41 @@
+package com.plcoding.cryptotracker.crypto.presentation.coin_list
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.CoinListItem
+import com.plcoding.cryptotracker.crypto.presentation.model.CoinListState
+
+@Composable
+fun CoinListScreen(
+    state: CoinListState,
+    modifier: Modifier = Modifier
+) {
+    if (state.isLoading) {
+        Box(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(state.coins) { coin ->
+                CoinListItem(coinUi = coin, onclick = { })
+                HorizontalDivider()
+
+            }
+        }
+    }
+}
